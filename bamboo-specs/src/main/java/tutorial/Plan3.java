4a544513afd25354f9fc6295dcffe39914948505
Anitha Edith <anithaedith@AESM42NLPC1239.local>
2022-12-07 18:22:19 +0530

    added another plan

--- bamboo-specs/src/main/java/tutorial/Plan3.java
+++ bamboo-specs/src/main/java/tutorial/Plan3.java
@@ -1,26 +1,24 @@
 package tutorial;
 
 import com.atlassian.bamboo.specs.api.BambooSpec;
-import com.atlassian.bamboo.specs.api.builders.permission.PlanPermissions;
 import com.atlassian.bamboo.specs.api.builders.plan.Plan;
-import com.atlassian.bamboo.specs.util.BambooServer;
 
 /**
  * Plan configuration for Bamboo.
  * Learn more on: <a href="https://confluence.atlassian.com/display/BAMBOO/Bamboo+Specs">https://confluence.atlassian.com/display/BAMBOO/Bamboo+Specs</a>
  */
 @BambooSpec
-public class Plan2 extends AbstractPlanSpec {
+public class Plan3 extends AbstractPlanSpec {
 
     public static void main(final String[] args)  {
-        Plan plan = new Plan2().createPlan();
+        Plan plan = new Plan3().createPlan();
         publishPlan(plan);
     }
 
     Plan createPlan() {
         return new Plan(
                 project(),
-                "Plan Name", "PLANKEY2")
+                "Plan Name", "PLANKEY3")
                 .description("Plan created from (enter repository url of your plan)");
     }
 
